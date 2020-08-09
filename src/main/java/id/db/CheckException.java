52127c82c7bee3f99bf1bf5893c25ed3a6d5301c
Igor Demura <igor.demura@gmail.com>
2020-08-08 21:34:56 -0700

    Change code style

---
+++ src/main/java/id/db/CheckException.java
@@ -0,0 +1,11 @@
+package id.db;
+
+public class CheckException extends RuntimeException
+{
+  public CheckException() {}
+
+  public CheckException(String description)
+  {
+    super(description);
+  }
+}
--- src/main/java/id/db/Main.java
+++ src/main/java/id/db/Main.java
@@ -3,15 +3,18 @@ package id.db;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 
-public final class Main {
+public final class Main
+{
   @Parameter(names = {"--user", "-u"})
   String user = "default";
 
-  void run() {
+  void run()
+  {
     System.out.println("user=" + user);
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+  {
     var main = new Main();
     JCommander.newBuilder().addObject(main).build().parse(args);
     main.run();
